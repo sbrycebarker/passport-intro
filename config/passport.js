840f19cc8464f4080627da49db164e2e86a6e015
bencallis1 <bencallis1@gmail.com>
2016-07-12 17:50:58 -0600

    another quick update

---
+++ config/passport.js
@@ -0,0 +1,11 @@
+// Require local strategy
+// Require user model
+
+
+// module.exports fucntion(passport)
+
+
+//  serialize the user for the session
+
+
+//  deserialize the user for the session
--- models/UserModel.js
+++ models/UserModel.js
@@ -10,14 +10,11 @@ var UserSchema = new mongoose.Schema({
 
 
 // methods ======================
-// generating a hash
-UserSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
 
-// checking if password is valid
-UserSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-};
+
+// generate hash
+
+//compareSync password
+
 
 module.exports = mongoose.model('User', UserSchema);
